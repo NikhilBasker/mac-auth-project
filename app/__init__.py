858a121783c7e54c232810c2b6726dbbb9eb40d3
NikhilBasker <124427705+NikhilBasker@users.noreply.github.com>
2025-01-17 19:57:44 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 # Initialize PyMongo
@@ -8,11 +9,14 @@ def create_app():
     app = Flask(__name__)
 
     # MongoDB connection URI
-    app.config['MONGO_URI'] = "mongodb+srv://nikhilsaravanan944:nikhi09@mac-auth.foem3.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=mac-auth"
+    app.config['MONGO_URI'] = "mongodb+srv://nikhilsaravanan944:nikhi09@mac-auth.foem3.mongodb.net/?retryWrites=true&w=majority&appName=mac-auth"
     app.config['SECRET_KEY'] = 'my$up3r$3cur3K3y'  # Your secret key
 
     # Initialize PyMongo
     mongo.init_app(app)
+
+    # Enable CORS
+    CORS(app)
 
     # Import and register routes
     from .routes import api_bp
